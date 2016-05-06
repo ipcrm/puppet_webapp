c70eeb1504f383ef7de1123fe5bb83104bf26bb0
ipcrm <mcadorette@puppetlabs.com>
2016-05-06 10:15:18 -0400

    demo setup

--- webui/views.py
+++ webui/views.py
@@ -3,9 +3,6 @@ from webui import webui
 
 @webui.route('/')
 @webui.route('/index')
-@webui.route('/<tagline>')
-def index(tagline='software'):
-    return render_template('index.html', tagline=tagline)
 
 @webui.errorhandler(404)
 def page_not_found(e):
