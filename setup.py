9e56252a6b9236855837c90337a09eb7686571ae
ipcrm <mcadorette@puppetlabs.com>
2016-05-06 15:56:06 -0400

    fixed setup.py reqs

--- setup.py
+++ setup.py
@@ -24,24 +24,11 @@ setup(
 
     # Dependent packages (distributions)
     install_requires=[
-      'alembic==0.7.7',
-      'blinker==1.4',
       'Flask==0.10.1',
-      'Flask-Alembic==1.2.1',
-      'Flask-Login==0.3.0',
-      'Flask-Mail==0.9.1',
-      'Flask-Principal==0.4.0',
-      'Flask-Security==1.7.4',
-      'Flask-SQLAlchemy==2.0',
       'Flask-Testing==0.4.2',
-      'Flask-WTF==0.12',
       'itsdangerous==0.24',
       'Jinja2==2.8',
-      'Mako==1.0.2',
       'MarkupSafe==0.23',
-      'passlib==1.6.5',
-      'SQLAlchemy==1.0.8',
-      'Werkzeug==0.10.4',
-      'WTForms==2.0.2',
+      'Werkzeug==0.11.9',
     ],
 )
