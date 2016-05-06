2132cb35def251376b138f753a050ac48e9c482c
ipcrm <mcadorette@puppetlabs.com>
2016-05-06 15:51:44 -0400

    Removed unneeded baggage

--- auth/config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-DEBUG = True
-TESTING = False
-CSRF_ENABLED = True
-SECRET_KEY = 'RmF1QWFoY1QxMjBUbmJRNWRNYzBvT21VRDU3T0dM'
-SQLALCHEMY_DATABASE_URI = "postgresql://puppet:puppet@localhost/puppet"
--- auth/views.py
+++
@@ -1,3 +0,0 @@
-from flask import render_template
-from flask import Flask
-from flask.ext.sqlalchemy import SQLAlchemy
