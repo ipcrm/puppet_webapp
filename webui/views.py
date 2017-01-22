cc5ea73e7ccfacb24732ef75f9df27c150479359
ipcrm <mcadorette@gmail.com>
2017-01-22 15:23:01 -0500

    Updating version string; bumped package version

--- webui/views.py
+++ webui/views.py
@@ -1,11 +1,13 @@
 from flask import render_template
 from webui import webui
+import pkg_resources 
+version = pkg_resources.require("webui")[0].version
 
 @webui.route('/')
 @webui.route('/index')
 @webui.route('/<tagline>')
 def index(tagline='software'):
-    return render_template('index.html', tagline=tagline)
+    return render_template('index.html', tagline=tagline, version=version)
 
 @webui.errorhandler(404)
 def page_not_found(e):
