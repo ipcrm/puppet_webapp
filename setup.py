371b3133cb7b8f606b03380ad45d5a443164adac
ipcrm <mcadorette@gmail.com>
2017-01-22 15:37:21 -0500

    Fixing Version String Line

--- setup.py
+++ setup.py
@@ -1,9 +1,17 @@
 from setuptools import setup
+import re
 
 try:
-  verstr = open('webui/_version.py', "rt").read()
+  verstrline = open('webui/_version.py', "rt").read()
 except:
   raise RuntimeError("cannot read version file")
+else:
+    VSRE = r"^__version__ = ['\"]([^'\"]*)['\"]"
+    mo = re.search(VSRE, verstrline, re.M)
+    if mo:
+        verstr = mo.group(1)
+    else:
+        raise RuntimeError("unable to find version in yourpackage/_version.py")
 
 setup(
     # Application name:
