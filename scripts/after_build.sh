f60f586bc6cf40a42ba4cb9f7c459fb536d15219
ipcrm <mcadorette@gmail.com>
2017-10-04 21:53:00 -0400

    test

--- scripts/after_build.sh
+++ scripts/after_build.sh
@@ -1,10 +1,11 @@
 echo "Build Success"
 echo "Creating github release"
 API_JSON=$(printf '{"tag_name":"%s","target_commitish":"%s","name":"%s","body":"Release of version %s","draft":false,"prerelease":false}' $DISTELLI_BUILDNUM $DISTELLI_RELBRANCH $DISTELLI_BUILDNUM $DISTELLI_BUILDNUM)
-curl -s -f -k -X POST -H "Content-Type: application/json" "https://api.github.com/repos/$GITHUB_USERNAME/$GITHUB_APPNAME/releases?access_token=$GITHUB_TOKEN" -d "$API_JSON"
+REL_ID=$(curl -s -f -k -X POST -H "Content-Type: application/json" "https://api.github.com/repos/$GITHUB_USERNAME/$GITHUB_APPNAME/releases?access_token=$GITHUB_TOKEN" -d "$API_JSON"|jq '.["id"]'|tr -d '\n')
+
 echo "Uploading artifact to github release"
 ARCHIVE=$(find . -name '*.tar.gz')
-GH_ASSET="https://uploads.github.com/repos/$GITHUB_USERNAME/$GITHUB_APPNAME/releases/$DISTELLI_BUILDNUM/assets?name=$(basename $ARCHIVE)"
+GH_ASSET="https://uploads.github.com/repos/$GITHUB_USERNAME/$GITHUB_APPNAME/releases/$REL_ID/assets?name=$(basename $ARCHIVE)&access_token=$GITHUB_TOKEN"
 echo $GH_ASSET
 echo $ARCHIVE
-curl --data-binary @$ARCHIVE --header "Authorization: token $GITHUB_TOKEN" $GH_ASSET
+curl --data-binary @"${ARCHIVE}" --header "Content-Type: application/x-gzip" $GH_ASSET
