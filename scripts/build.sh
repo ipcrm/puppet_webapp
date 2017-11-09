a8aaa3d2eb2a0e2fb35d64bea2496970a17180b8
ipcrm <mcadorette@gmail.com>
2017-11-08 23:12:07 -0500

    testing just 2 vers

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,6 @@
 export PYENV_ROOT="$HOME/.pyenv"
 export PATH="$PYENV_ROOT/bin:$PATH"
 eval "$(pyenv init -)"
-pyenv local 2.7.10 3.3.6 3.4.3 3.5.0
+pyenv local 2.7.10 3.5.0
 tox
 python ./setup.py sdist
--- scripts/pre_build.sh
+++ scripts/pre_build.sh
@@ -5,8 +5,6 @@ export PYENV_ROOT="$HOME/.pyenv"
 export PATH="$PYENV_ROOT/bin:$PATH"
 eval "$(pyenv init -)"
 pyenv install 2.7.10
-pyenv install 3.3.6
-pyenv install 3.4.3
 pyenv install 3.5.0
-pyenv local 2.7.10 3.3.6 3.4.3 3.5.0
+pyenv local 2.7.10 3.5.0
 pip3.5 install tox
